d5b4975fce60d9fbfa4004f634a654bba4d77e6c
Ehtii <janjua.ehtasham@gmail.com>
2022-10-02 12:41:42 +0200

    utilityfuncitons

---
+++ utils/helpers.py
@@ -0,0 +1,83 @@
+import cv2
+import numpy as np
+import itertools
+import operator
+import os, csv
+import tensorflow as tf
+
+import time, datetime
+
+def get_label_info(csv_path):
+    filename, file_extension = os.path.splitext(csv_path)
+    if not file_extension == ".csv":
+        return ValueError("File is not a CSV!")
+
+    class_names = []
+    label_values = []
+    with open(csv_path, 'r') as csvfile:
+        file_reader = csv.reader(csvfile, delimiter=',')
+        header = next(file_reader)
+        for row in file_reader:
+            class_names.append(row[0])
+            label_values.append([int(row[1]), int(row[2]), int(row[3])])
+    return class_names, label_values
+
+
+def one_hot_it(label, label_values):
+    """
+    Convert a segmentation image label array to one-hot format
+    by replacing each pixel value with a vector of length num_classes
+
+    # Arguments
+        label: The 2D array segmentation image label
+        label_values
+        
+    # Returns
+        A 2D array with the same width and hieght as the input, but
+        with a depth size of num_classes
+    """
+    semantic_map = []
+    for colour in label_values:
+        # colour_map = np.full((label.shape[0], label.shape[1], label.shape[2]), colour, dtype=int)
+        equality = np.equal(label, colour)
+        class_map = np.all(equality, axis = -1)
+        semantic_map.append(class_map)
+    semantic_map = np.stack(semantic_map, axis=-1)
+    
+    return semantic_map
+    
+def reverse_one_hot(image):
+    """
+    Transform a 2D array in one-hot format (depth is num_classes),
+    to a 2D array with only 1 channel, where each pixel value is
+    the classified class key.
+
+    # Arguments
+        image: The one-hot format image 
+        
+    # Returns
+        A 2D array with the same width and hieght as the input, but
+        with a depth size of 1, where each pixel value is the classified 
+        class key.
+    """
+    x = np.argmax(image, axis = -1)
+    return x
+
+
+def colour_code_segmentation(image, label_values):
+    """
+    Given a 1-channel array of class keys, colour code the segmentation results.
+
+    # Arguments
+        image: single channel array where each value represents the class key.
+        label_values
+        
+    # Returns
+        Colour coded image for segmentation visualization
+    """
+
+    colour_codes = np.array(label_values)
+    x = colour_codes[image.astype(int)]
+
+    return x
+
